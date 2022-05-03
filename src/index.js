0ce592929b94fe20e84b801b1588784e289a5f62
danburudoa <yumamukai515@gmail.com>
2022-05-03 18:07:24 +0900

    App.jsの関数をindex.jsでも使えるようにした

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import { Fragment } from "react";
+import { App } from "./App";
+
+//ReactDOMには関数が用意されている。第一引数の<App />は関数名をさし第二引数は、renderの箇所を設定する。
+ReactDOM.render(<App />, document.getElementById("root"));
