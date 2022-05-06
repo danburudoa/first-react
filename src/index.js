90bcd60320aaa61ff3e24552ac01367267df41d9
danburudoa <yumamukai515@gmail.com>
2022-05-06 17:35:50 +0900

    参照したいコンポーネントを囲む

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import ReactDOM from "react-dom";
+
 import { App } from "./App";
+import { AdminFlagProvider } from "./components/providers/AdminFlagProvider";
+
 
 //ReactDOMには関数が用意されている。第一引数の<App />は関数名をさし第二引数は、renderの箇所を設定する。
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AdminFlagProvider>
+    <App />
+  </AdminFlagProvider>,
+  document.getElementById("root")
+);
