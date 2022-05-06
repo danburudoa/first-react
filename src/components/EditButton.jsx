711b121ceb2e9b5ec443f307e6db055b3a5fb2e6
danburudoa <yumamukai515@gmail.com>
2022-05-06 17:49:15 +0900

    動作確認

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,3 +1,9 @@
+//reactからuseContextimport
+import { useContext } from "react";
+
+// 作成したContextをimport
+import { AdminFlagContext } from "./providers/AdminFlagProvider";
+
 const style = {
   width: "100px",
   padding: "6px",
@@ -6,7 +12,11 @@ const style = {
 
 export const EditButton = props => {
   const { isAdmin } = props;
-  // isAdminがfalse(管理者でない)時にボタンを非活性にする
+
+  // useContextの引数に参照するContextを指定する
+  const contextValue = useContext(AdminFlagContext);
+  console.log(contextValue); // {sampleValue: "テスト"}
+  
   return (
     <button style={style} disabled={!isAdmin}>
       編集
