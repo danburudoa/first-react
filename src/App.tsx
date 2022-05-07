7147abe57fe78bb69dbb50780c0683d015868bd8
danburudoa <yumamukai515@gmail.com>
2022-05-07 15:05:29 +0900

    Propsへの型定義

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import { useEffect, useState } from "react";
 import { ListItem } from "./components/ListItem";
 import axios from 'axios';
 
+// ユーザー情報の型定義
+type User = {
+  id: number;
+  name: string;
+  age: number;
+  personalColor: string;
+}
+
 export const App = () => {
   // 取得したユーザー情報
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   // 画面表示時にユーザー情報取得
   useEffect(() => {
-    axios.get("https://example.com/users").then((res) => {
+    axios.get<User[]>("https://example.com/users").then((res) => {
       setUsers(res.data);
     })
   }, []);
@@ -17,7 +25,7 @@ export const App = () => {
   return (
     <div>
       {users.map(user => (
-        <ListItem id={user.id} name={user.name} age={user.age} />
+        <ListItem id={user.id} name={user.nama} age={user.age} personalColor={user.personalColor} />
       ))}
     </div>
   );
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,8 +1,17 @@
-export const ListItem = () => {
-  const { id, name, age } = props;
+// Propsの型を定義
+type User = {
+  id: number;
+  name: string;
+  age: number;
+  personalColor: string;
+}
+
+// Propsに型を指定
+export const ListItem = (props: User) => {
+  const { id, name, age, personalColor } = props;
 
   return (
-    <p>
+    <p style={{ color: personalColor }}>
       {id}:{name}({age})
     </p>
   );
